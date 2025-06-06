93bacec6dd0a7a7decf2899448f0d4431da065cc
pTinosq <tinosps@gmail.com>
2025-06-06 11:06:44 +0100

    Update test to match new test data

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, "src/")
 from pixelsparser import utils  # noqa: E402
 from datetime import datetime  # noqa: E402
@@ -7,24 +8,27 @@ from datetime import datetime  # noqa: E402
 def test_load():
     pixels = utils.load("tests/test_data/test_diary.json")
 
-    assert len(pixels) == 2
+    assert len(pixels) == 4
     assert pixels[0].date == datetime.strptime("2022-09-20", "%Y-%m-%d")
     assert pixels[0].pixelType == "Mood"
     assert pixels[0].scores == [2]
-    assert pixels[0].notes == "I accidentally ate a whole bag of chips today."\
+    assert (
+        pixels[0].notes == "I accidentally ate a whole bag of chips today."
         " It slipped into my mouth. I swear!"
-    assert pixels[0].tags == [{
-        "type": "Emotions", "entries": ["joy", "sadness", "fear", "disgust"]}
-                              ]
+    )
+    assert pixels[0].tags == [
+        {"type": "Emotions", "entries": ["joy", "sadness", "fear", "disgust"]}
+    ]
     assert pixels[0].score == 2
     assert pixels[0].mood == 2
 
     assert pixels[1].date == datetime.strptime("2022-09-21", "%Y-%m-%d")
     assert pixels[1].pixelType == "Mood"
     assert pixels[1].scores == [3]
-    assert pixels[1].notes == "I'm feeling pretty good today. I'm going to"\
+    assert (
+        pixels[1].notes == "I'm feeling pretty good today. I'm going to"
         " try to eat better."
-    assert pixels[1].tags == [{
-        "type": "Emotions", "entries": ["joy", "disgust"]}]
+    )
+    assert pixels[1].tags == [{"type": "Emotions", "entries": ["joy", "disgust"]}]
     assert pixels[1].score == 3
     assert pixels[1].mood == 3
